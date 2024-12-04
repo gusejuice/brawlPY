6e346792099612f2903a6bef8d8acf187f2e7810
gusejuice <jbsnowden05@icloud.com>
2024-12-03 23:14:29 -0500

    gets club json now

--- brawlPY/brawlPY.py
+++ brawlPY/brawlPY.py
@@ -1,4 +1,4 @@
-try:        
+try: # if intalling brawlPY from pip, this should happen automatically
     from dotenv import load_dotenv
     from requests import get
 except ImportError:
@@ -9,7 +9,7 @@ class AuthorizationError(Exception):
     pass
 
 class APIClient:
-    def __init__(self, key = None):
+    def __init__(self, key: str = None):
         # looks for api key in .env file and if there is no key provided in param
         load_dotenv()
         self.key = os.getenv("API_KEY") or key
@@ -26,26 +26,26 @@ class APIClient:
         else:
             raise AuthorizationError(responce.json())
     
-    # methods that return json of specific data from the api
+    # methods that return json of specific data from the api. some of these are useless i know
     def get_brawler_list(self):
         return self.__get_json("https://api.brawlstars.com/v1/brawlers")
 
-    def get_brawler_info(self, tag):
+    def get_brawler_info(self, tag: str):
         return self.__get_json(f"https://api.brawlstars.com/v1/brawlers/{tag}")
 
-    def get_club_info(self, tag):
+    def get_club_info(self, tag: str):
         return self.__get_json(f"https://api.brawlstars.com/v1/clubs/%23{tag}")
 
-    def get_club_members(self, tag):
+    def get_club_members(self, tag: str):
         return self.__get_json(f"https://api.brawlstars.com/v1/clubs/%23{tag}/members")
 
     def get_event_list(self):
         return self.__get_json("https://api.brawlstars.com/v1/events/rotation")
 
-    def get_player_battlelog(self, tag):
+    def get_player_battlelog(self, tag: str):
         return self.__get_json(f"https://api.brawlstars.com/v1/players/%23{tag}/battlelog")
 
-    def get_player_info(self, tag):
+    def get_player_info(self, tag: str):
         return self.__get_json(f"https://api.brawlstars.com/v1/players/%23{tag}")
 
 class Player:
@@ -56,40 +56,40 @@ class Player:
         # creates a class for each brawler that is available in the "brawler" section of the player's json file
         self.brawler = {brawler["name"].lower(): self.Brawler(brawler) for brawler in self.__json["brawlers"]}
 
-    def getName(self): # returns a string of the nickname that the player is using
+    def getName(self) -> str: # returns a string of the nickname that the player is using
         return self.__json["name"]
     
-    def getTrophies(self): # returns an integer of the current amount of trophies
+    def getTrophies(self) -> int: # returns an integer of the current amount of trophies
         return self.__json["trophies"]
     
-    def getExpLevel(self): # returns an integer of the current level
+    def getExpLevel(self) -> int: # returns an integer of the current level
         return self.__json["expLevel"]
     
-    def getExpPoints(self): 
+    def getExpPoints(self) -> int: 
         return self.__json["expPoints"]
     
-    def getHighestTrophies(self): # returns an integer the highest trophy count
+    def getHighestTrophies(self) -> int: # returns an integer the highest trophy count
         return self.__json["highestTrophies"]
     
-    def isQualifiedFromChampionshipChallenge(self): # returns a boolean 
+    def isQualifiedFromChampionshipChallenge(self) -> bool: # returns a boolean 
         return self.__json["isQualifiedFromChampionshipChallenge"]
     
-    def getThreeVictories(self): # returns an integer of current total of 3v3 victories
+    def getThreeVictories(self) -> int: # returns an integer of current total of 3v3 victories
         return self.__json["3vs3Victories"]
 
-    def getSoloVictories(self): # returns an integer of current total of solo showdown victories
+    def getSoloVictories(self) -> int: # returns an integer of current total of solo showdown victories
         return self.__json["soloVictories"]
     
-    def getDuoVictories(self): # returns an integer of current total of duo showdown victories
+    def getDuoVictories(self) -> int: # returns an integer of current total of duo showdown victories
         return self.__json["duoVictories"]
     
-    def getBestRoboRumbleTime(self): 
+    def getBestRoboRumbleTime(self) -> int: 
         return self.__json["bestRoboRumbleTime"]
     
-    def getBestTimeAsBigBrawler(self):
+    def getBestTimeAsBigBrawler(self) -> int:
         return self.__json["bestTimeAsBigBrawler"]
 
-    def getNameColor(self): # returns a color code of the users name color
+    def getNameColor(self): # returns a color code of the user's name color
         try:
             return self.__json["nameColor"]
         except KeyError:
@@ -125,7 +125,7 @@ class Player:
         
 class Club:
     def __init__(self, tag: str, client: APIClient):
-        self.__json = client.get_player_info(tag)
+        self.__json = client.get_club_info(tag)
     
     def getName(self): # returns a string of the club name
         return self.__json["name"]
